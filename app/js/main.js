6022e816079e497e7ca052abca72ad7d7c3a9e2e
nick320077 <kobets32@gmail.com>
2020-11-19 13:17:15 +0200

    added top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
- 
+ $('.top-slider__inner').slick({
+dots: true,
+arrows: false,
+fade: true,
+autoplay: true,
+autoplaySpeed: 2000
+
+
+ });
 });
+
+// document.querySelector('.top-slider__inner');
