429d1002f07b9060c0f0a43011b880bd59465151
nick320077 <kobets32@gmail.com>
2020-11-21 15:59:42 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -5,9 +5,13 @@ arrows: false,
 fade: true,
 autoplay: true,
 autoplaySpeed: 2000
-
-
- });
 });
 
-// document.querySelector('.top-slider__inner');
+$(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+});
+
